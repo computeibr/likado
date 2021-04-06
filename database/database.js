7f8426b986d64bfadb9a22482dfbaa4c1b06a0c6
computeibr <leonardo.computei@gmail.com>
2021-04-06 14:21:16 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize('computei_comando','computei_comando','Educacao100',{
-    host: '138.128.185.195',
+    host: 'mysql743.umbler.com:41890',
     dialect: 'mysql',
     timezone: "-03:00"
 });
